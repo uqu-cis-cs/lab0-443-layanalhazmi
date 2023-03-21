fddf001cf8eb4c8794a7da51997da81ddb5b1e0c
layanalhazmi <120459134+layanalhazmi@users.noreply.github.com>
2023-03-21 20:29:27 +0000

    first project

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -22,72 +22,77 @@ public class App{
 *        1 Tweetle 3 TweetleBeetle 5 TweetlePoodle 7 TweetleBeetle 9 Tweetle 11
 *        TweetleBeetlePoodle 13 Tweetle 15 TweetleBeetle 17 TweetlePoodle 19 TweetleBeetle 
 */
-public static void twisters(){;
-   for(int i=1;i<=110;i++){
-     if(i%2==0){
-        System.out.print("Tweetle");
-        if(i%4==0){
-            System.out.print("Beetle");
+public static void twisters() {
+
+        for (int i = 1; i <= 110; i++) {
+
+            if (i % 2 == 0) {
+                System.out.print("Tweetle");
+
+                if (i % 4 == 0) {
+                    System.out.print("Beetle");
+                }
+
+                if (i % 6 == 0) {
+                    System.out.print("Poodle");
+                }
+            }
+            else {
+                System.out.print(" " + i + " ");
+            }
         }
-        if(i%6==0){
-            System.out.print("poodle");
-        }
-     }
-     else{
-        System.out.print(" "+i+" ");
-     }
-   }
 }
-public static void phoneKeypad(String text){
-    String result=" ";
-    for(char ch:text.toUpperCase().toCharArray()){
-        switch (ch){
-            case 'A':
-            case 'B':
-            case 'c':
-              result +="2";
-              break;
-            case 'D':
-            case 'E':
-            case 'F':
-              result+="3";
-              break;
-            case 'G':
-            case 'H':
-            case 'I':
-               result +="4";
-               break;
-            case 'J':
-            case 'K':
-            case 'L':
-              result +="5";
-              break;
-            case 'M':
-            case 'N':
-            case 'O':
-               result+="6";
-               break;
-            case 'P':
-            case 'Q':
-            case 'R':
-            case 'S':
-              result+="7";
-               break;
-            case 'T':
-            case 'U':
-            case 'V':
-               result+="8";
-               break;
-            case 'W':
-            case 'X':
-            case 'Y':
-            case 'Z':
-               result+="9";
-               break;
-        }
-    }
-    System.out.println(result);
+public static void phoneKeypad(String text) {
+   String result = "";
+     for (char ch : text.toUpperCase().toCharArray()) {
+        switch (ch) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    result += "2";
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    result += "3";
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    result += "4";
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    result += "5";
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    result += "6";
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    result += "7";
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    result += "8";
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    result += "9";
+                    break;
+          }
+      }
+      System.out.println(result);
 }
+
 /**
 * Write a method named "phoneKeypad" that takes a string parameter (str)
 * then it replaces each letter in (str) by a number that represnts it 
@@ -112,7 +117,7 @@ public static void phoneKeypad(String text){
         
           twisters();
           Scanner input=new Scanner(System.in);
-          System.out.print("Enter a string");
+          System.out.print("Enter a string>>");
           phoneKeypad(input.nextLine());
         /* Write your code here */
      
@@ -121,6 +126,6 @@ public static void phoneKeypad(String text){
        //call method phoneKeypad(string)
 
 
-    }
+   }
 
 }
